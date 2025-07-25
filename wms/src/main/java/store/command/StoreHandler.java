e8380ba80d293cd26d4b7b3207af0deaf7198318
User <User@DESKTOP-E377OPC>
2025-07-25 17:09:18 +0900

    ---

---
+++ wms/src/main/java/store/command/StoreHandler.java
@@ -0,0 +1,16 @@
+package store.command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import mvc.command.CommandHandler;
+
+public class StoreHandler implements CommandHandler {
+
+	@Override
+	public String process(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
