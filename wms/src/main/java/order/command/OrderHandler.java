ffabbc478c8b27ecfa7cdaa3d277284a4c041491
User <ayhwork0711@gmail.com>
2025-08-03 17:35:43 +0900

    OrderHandler added

--- wms/src/main/java/order/command/OrderHandler.java
+++ wms/src/main/java/order/command/OrderHandler.java
@@ -1,18 +1,22 @@
 package order.command;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import mvc.command.CommandHandler;
+import order.model.Order;
+import order.service.OrderListService;
 
 public class OrderHandler implements CommandHandler {
 
 	@Override
 	public String process(HttpServletRequest req, HttpServletResponse res) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-		
-		//test5
-	}
+	OrderListService orderListService = new OrderListService();	
+	List<Order> orderList = orderListService.getOrderList();
+	req.setAttribute("orderList", orderList);
+	return "/WEB-INF/view/orderList.jsp";
+}
 }
 
