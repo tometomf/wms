8132402b852af94ce0602b7bc646781340145bd0
moonchaewon <houngmsh@naver.com>
2025-08-20 19:11:49 +0900

    .제거

--- wms/src/main/java/item/command/ItemInsertHandler.java
+++ wms/src/main/java/item/command/ItemInsertHandler.java
@@ -60,7 +60,7 @@ public class ItemInsertHandler implements CommandHandler {
 			res.setContentType("text/html; charset=UTF-8");
 			PrintWriter out = res.getWriter();
 			out.println("<script>");
-			out.println("alert('登録できました。.');");
+			out.println("alert('登録できました。');");
 			out.println("location.href='list.do';");
 			out.println("</script>");
 			out.close();
